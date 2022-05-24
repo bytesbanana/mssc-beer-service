9ab3428e0d8a18df9be5c75f9b5da0c67ed253a4
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-05-24 21:46:53 +0700

    Add README.md and update main apps name

---
+++ README.md
@@ -0,0 +1,3 @@
+#MSSC Beer service
+
+Spring example webservice
--- src/main/java/com/bytesbanana/msscbeerservice/MSSCBeerServiceApplication.java
+++ src/main/java/com/bytesbanana/msscbeerservice/MSSCBeerServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsscBeerServiceApplication {
+public class MSSCBeerServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsscBeerServiceApplication.class, args);
+        SpringApplication.run(MSSCBeerServiceApplication.class, args);
     }
 
 }
